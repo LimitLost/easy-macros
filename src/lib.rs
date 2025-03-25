c8dc971e8fc0c1d75a6e922df28f781f209e8a31
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-03-25 11:10:36 +0100

    Turn easy-lib for procedural macros into a feature "macros"

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
+#[cfg(feature = "for-macro")]
 pub mod macros {
+    pub use all_syntax_cases::*;
     pub use always_context::*;
     pub use attributes::*;
+    pub use macro_result::*;
 }
 
-pub use helpers;
+#[cfg(feature = "for-macro")]
+pub use {helpers, proc_macro2, quote, syn};
+
+#[cfg(all(feature = "general", not(feature = "for-macro")))]
+pub mod helpers {
+    pub use helpers_context::*;
+}
+
+#[cfg(all(feature = "general", not(feature = "for-macro")))]
+pub mod macros {
+    pub use always_context::*;
+}
 
 #[cfg(test)]
 mod macro_tests;
