b01c99e99831b6550223895bfda405b4e6afc407
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-03-25 09:12:09 +0100

    get_attributes Macro complete! (and tested)

--- proc_macro_tests/src/lib.rs
+++ proc_macro_tests/src/lib.rs
@@ -1,14 +1,17 @@
-use helpers::MacroResult;
-use macros::has_attributes;
+use anyhow::Context;
+use attributes::{get_attributes, has_attributes};
+use helpers::{MacroResult, context};
 use proc_macro::TokenStream;
+use quote::ToTokens;
 use quote::quote;
 use syn::ItemStruct;
 
-#[proc_macro_derive(DeriveTestStruct)]
-pub fn macro_test_struct(item: TokenStream) -> TokenStream {
-    let parsed = syn::parse_macro_input!(item as syn::ItemStruct);
+#[proc_macro_derive(DeriveTestStruct, attributes(lol, lmao, xlold, xdedd, xnoned, xb, bbb))]
+#[macro_result::macro_result]
+pub fn attributes_test_struct(item: TokenStream) -> anyhow::Result<TokenStream> {
+    let parsed = helpers::parse_macro_input!(item as syn::ItemStruct);
 
-    let mut result = MacroResult::new();
+    let mut result = MacroResult::default();
 
     if !has_attributes!(parsed, #[lol]) {
         //Show Compiler error
@@ -17,10 +20,107 @@ pub fn macro_test_struct(item: TokenStream) -> TokenStream {
         });
     }
 
-    result.finalize().into()
-}
+    let mut lol_found = false;
+    let mut ded_found = false;
+    let mut none_found = false;
 
-//TODO get_attribute
+    for data in get_attributes!(parsed,#[lmao]#[x__unknown__d]).into_iter() {
+        match data
+            .to_string()
+            .replace(|c: char| c.is_whitespace(), "")
+            .as_str()
+        {
+            "lol" => lol_found = true,
+            "ded" => ded_found = true,
+            "none" => none_found = true,
+            i => {
+                let f = format!(
+                    "(#[lmao]#[x__unknown__d]) Unexpected attribute on struct: {}",
+                    i
+                );
+                //Show Compiler error
+                result.add(quote! {
+                    compile_error!(#f);
+                });
+            }
+        }
+    }
+
+    if !lol_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[lmao]#[xlold] attribute on struct not found");
+        });
+    }
+
+    if !ded_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[lmao]#[xdedd] attribute on struct not found");
+        });
+    }
+
+    if !none_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[lmao]#[xnoned] attribute on struct not found");
+        });
+    }
+    let mut special_found = false;
+    let mut dollars_found = false;
+    let mut eq_found = false;
+    let mut lul_found = false;
+
+    for data in get_attributes!(parsed,#[bbb((lol__unknown__X))]).into_iter() {
+        match data
+            .to_string()
+            .replace(|c: char| c.is_whitespace(), "")
+            .as_str()
+        {
+            "special" => special_found = true,
+            "$$$" => dollars_found = true,
+            "=" => eq_found = true,
+            "((lul))" => lul_found = true,
+            i => {
+                let f = format!("(lol__unknown__X) Unexpected attribute on struct: {}", i);
+                //Show Compiler error
+                result.add(quote! {
+                    compile_error!(#f);
+                });
+            }
+        }
+    }
+
+    if !special_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[bbb((lolspecialX))] attribute on struct not found");
+        });
+    }
+
+    if !dollars_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[bbb((lol$$$X))] attribute on struct not found");
+        });
+    }
+
+    if !eq_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[bbb((lol=X))] attribute on struct not found");
+        });
+    }
+
+    if !lul_found {
+        //Show Compiler error
+        result.add(quote! {
+            compile_error!("#[bbb((lol((lul))X))] attribute on struct not found");
+        });
+    }
+
+    Ok(result.finalize().into())
+}
 
 ///Expected Struct:
 /// struct TestStruct{
@@ -30,7 +130,7 @@ pub fn macro_test_struct(item: TokenStream) -> TokenStream {
 pub fn macro_test_eq(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let parsed = syn::parse_macro_input!(item as syn::ItemStruct);
 
-    let mut result = MacroResult::new();
+    let mut result = MacroResult::default();
 
     let not_real_struct: ItemStruct = syn::parse_quote! {
         struct TestStruct{
--- src/macro_tests/mod.rs
+++ src/macro_tests/mod.rs
@@ -1,21 +1,38 @@
 use all_syntax_cases::all_syntax_cases;
-use proc_macro_tests::macro_test_eq;
+use proc_macro_tests::{DeriveTestStruct, macro_test_eq};
+use quote::ToTokens;
 
 #[macro_test_eq]
 struct TestStruct {
     field: i32,
 }
 //Tests if all default functions are used and alright (no error making mistakes inside of them)
-struct Nothing {
+#[derive(Debug, Clone)]
+struct _Nothing {
     x: i32,
 }
+
 all_syntax_cases! {
     setup => {
         generated_fn_prefix: "example",
-        additional_input_type: Nothing,
+        additional_input_type: _Nothing,
         //Used for debugging
         system_functions_test: true
     }
     default_cases => {}
     special_cases => {}
 }
+
+#[derive(DeriveTestStruct)]
+#[lol]
+#[lmao]
+#[xlold]
+#[xdedd]
+#[xnoned]
+#[xb = "cd"]
+#[bbb((lolspecialX))]
+#[bbb((lol=X))]
+#[bbb((lol = X))]
+#[bbb((lol$$$X))]
+#[bbb((lol((lul))X))]
+struct _AttributeTest {}
