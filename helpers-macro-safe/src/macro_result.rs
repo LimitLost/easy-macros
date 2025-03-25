48c574fa384ccfbdf168489aea88c6194dcc169e
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-03-25 09:17:23 +0100

    quick add braces to MacroResult

--- helpers-macro-safe/src/macro_result.rs
+++ helpers-macro-safe/src/macro_result.rs
@@ -8,6 +8,16 @@ impl MacroResult {
     pub fn add(&mut self, item: TokenStream) {
         self.result.extend(item);
     }
+    ///Wraps result with a pair of braces
+    pub fn braced(&mut self) {
+        replace_with::replace_with_or_abort(&mut self.result, |result| {
+            quote::quote! {
+                {
+                    #result
+                }
+            }
+        });
+    }
 
     pub fn finalize(self) -> TokenStream {
         self.result
