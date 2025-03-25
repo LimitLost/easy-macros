9b8e7e411720f45f6771da1fcdc7f8b0bb9a11d1
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-03-25 09:12:48 +0100

    fields_with_attributes Macro complete!

---
+++ attributes-macros/src/fields_with_attributes.rs
@@ -0,0 +1,62 @@
+use always_context::always_context;
+use helpers_macro_safe::{MacroResult, parse_macro_input};
+use quote::quote;
+
+use crate::{
+    data::{HandleMaybeRefAttrsInput, Reference},
+    root_macros_crate,
+};
+
+#[always_context]
+pub fn fields_with_attributes(
+    item: proc_macro::TokenStream,
+) -> anyhow::Result<proc_macro::TokenStream> {
+    let parsed = parse_macro_input!(item as HandleMaybeRefAttrsInput);
+
+    let operate_on = parsed.operate_on;
+    let attributes = parsed.attributes;
+    let mut result = MacroResult::default();
+
+    let (iter, maybe_fields, unit_handle) = match parsed.reference {
+        Some(Reference::Ref) => (
+            quote! { .iter() },
+            quote! { let f= syn::punctuated::Punctuated::new();},
+            quote! { &f },
+        ),
+        Some(Reference::RefMut) => (
+            quote! { .iter_mut() },
+            quote! { let f= syn::punctuated::Punctuated::new();},
+            quote! { &mut f },
+        ),
+        None => (
+            quote! { .into_iter() },
+            quote! {},
+            quote! {Default::default()},
+        ),
+    };
+
+    let crate_root = root_macros_crate();
+
+    result.add(quote! {
+        {
+            #maybe_fields
+            let fields=match #operate_on.fields{
+                syn::Fields::Named(fields) => {
+                    fields
+                }
+                syn::Fields::Unnamed(fields) => {
+                    fields
+                }
+                syn::Fields::Unit => {
+                    #unit_handle
+                }
+            };
+
+            fields #iter .filter(|field|{
+                #crate_root::has_attributes!(field,#(#attributes)*)
+            })
+        }
+    });
+
+    Ok(result.finalize().into())
+}
