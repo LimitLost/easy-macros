f5c1c512982a9ddd804e46d3db934d2c8216c746
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-11-17 07:14:30 +0100

    Scripts: Fix pr.sh not working from every directory

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./build_readme_docs.sh
-./readme_join.sh
-./test.sh
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+bash "$PROJECT_ROOT/scripts/build_readme_docs.sh"
+bash "$PROJECT_ROOT/scripts/readme_join.sh"
+bash "$PROJECT_ROOT/scripts/test_with_local_deps.sh"
