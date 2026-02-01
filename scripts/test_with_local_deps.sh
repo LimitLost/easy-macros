2a334a748efd014516c671f689a184316767c888
LimitLost <46265927+LimitLost@users.noreply.github.com>
2026-02-01 09:37:31 +0100

    Scripts: Fix add-code not being  updated by test_with_local_deps

--- scripts/test_with_local_deps.sh
+++ scripts/test_with_local_deps.sh
@@ -93,6 +93,7 @@ replace_versions_with_paths() {
         ["easy-macros-context-internal"]="context-internal"
         ["easy-macros-helpers"]="helpers"
         ["easy-macros-proc-macro-tests"]="proc_macro_tests"
+        ["easy-macros-add-code"]="add-code"
     )
     
     # Process each Cargo.toml file
